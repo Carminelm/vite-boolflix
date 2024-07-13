3ceae5ff0757b0a17bcf60248b3f1026c120f7e3
Carminelm <carminelm54@gmail.com>
2024-07-13 15:51:59 +0200

    app vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,40 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+
+import AppHeader from './components/AppHeader.vue';
+import { store } from './store.js';
+import axios from 'axios';
+
+export default {
+    components: {
+        AppHeader,
+
+    },
+    data() {
+        return {
+            store,
+        }
+    },
+    methods:{
+
+}
+
+
+}
+
 </script>
 
-<template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+<template lang="">
+
+    <div>
+        <AppHeader @search="searchMovie" />
+        <AppMain />
+    </div>
+
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style lang="scss">
+
+    @import './styles/generals.scss';
+    @import '../node_modules/bootstrap/scss/bootstrap';
+
 </style>
